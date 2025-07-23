01e1629cf0aa0ed5d5a7fcd23adbe8057e15181b
Abdul Nabeel M <abdulnabeel.m@prodapt.com>
2025-07-23 16:07:25 +0530

    Add Demo3

---
+++ Day1/src/com/Nabeel/Demo3.java
@@ -0,0 +1,13 @@
+package com.Nabeel;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		System.out.println(addTwoNum(6,7));
+	}
+	
+	public static int addTwoNum(int a,int b) {
+		return a+b;
+	}
+
+}
